e3839192a1db85fb6b3a89ada6abfc911da8d9ae
brenobonani <brenobonani@gmail.com>
2022-07-14 21:02:22 -0300

    deploying on heroku

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.get("/", function(req, res){
 });
 
 // app listen
-app.listen(process.env.PORT, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running");
 });
 
